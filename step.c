5976db2c2ab5cb3879f49d48f2adb4cb00b120e5
Chandan H <106821717+mr-chandan@users.noreply.github.com>
2023-07-09 18:02:33 +0530

    Update step.c

--- step.c
+++ step.c
@@ -1,70 +1,65 @@
 #include <stdio.h>
-#include <stdbool.h>
-#include <limits.h>
-
-#define V 6 // Number of vertices in the graph
-
-void primMST(int graph[V][V])
-{
-    int parent[V];
-    int key[V];
-    bool mstSet[V];
-
-    for (int i = 0; i < V; i++)
-    {
-        key[i] = INT_MAX;
-        mstSet[i] = false;
-    }
-
-    key[0] = 0;
-    parent[0] = -1;
-
-    for (int count = 0; count < V - 1; count++)
-    {
-        int min = INT_MAX, min_index;
-
-        for (int v = 0; v < V; v++)
-        {
-            if (mstSet[v] == false && key[v] < min)
-            {
-                min = key[v];
-                min_index = v;
-            }
-        }
-
-        int u = min_index;
-
-        mstSet[u] = true;
-
-        for (int v = 0; v < V; v++)
-        {
-            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
-            {
-                parent[v] = u;
-                key[v] = graph[u][v];
-            }
-        }
-    }
-
-    printf("Edge \tWeight\n");
-    for (int i = 1; i < V; i++)
-    {
-        printf("%d - %d \t%d\n", parent[i], i, graph[i][parent[i]]);
-    }
-}
+#include <stdlib.h>
 
 int main()
 {
-    int graph[V][V] = {
-        {0, 2, 6, 0, 0, 0},
-        {2, 0, 0, 5, 7, 0},
-        {6, 0, 0, 4, 0, 0},
-        {0, 5, 4, 0, 0, 3},
-        {0, 7, 0, 0, 0, 0},
-        {0, 0, 0, 3, 0, 0}
-    };
+    int len;
+    printf("Enter the number of nodes: ");
+    scanf("%d", &len);
 
-    primMST(graph);
+    int adj[len][len];
+    int parent[len], weight[len], visited[len];
+
+    printf("Enter the adjacency matrix:\n");
+    for (int i = 0; i < len; i++)
+    {
+        for (int j = 0; j < len; j++)
+        {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+
+    for (int j = 0; j < len; j++)
+    {
+        parent[j] = -1;
+        weight[j] = 999;
+        visited[j] = 0;
+    }
+
+    weight[0] = 0;
+
+    for (int i = 0; i < len; i++)
+    {
+        int min = 999;
+        int minIndex = -1;
+
+        for (int z = 0; z < len; z++)
+        {
+            if (visited[z] == 0 && weight[z] < min)
+            {
+                min = weight[z];
+                minIndex = z;
+            }
+        }
+
+        visited[minIndex] = 1;
+
+        for (int j = 0; j < len; j++)
+        {
+            if (adj[minIndex][j] != 0 && visited[j] == 0 && adj[minIndex][j] < weight[j])
+            {
+                parent[j] = minIndex;
+                weight[j] = adj[minIndex][j];
+            }
+        }
+    }
+
+    printf("Minimum Spanning Tree:\n");
+    printf("Edge \tWeight\n");
+    for (int i = 1; i < len; i++)
+    {
+        printf("%d - %d \t%d\n", parent[i], i, adj[parent[i]][i]);
+    }
 
     return 0;
 }
